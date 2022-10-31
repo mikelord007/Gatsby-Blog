846e8c568ad57d53e1d3535fff3a9abe764691ee
manu jasan <manujasan23@gmail.com>
2022-10-31 15:29:33 +0530

    added tailwindui components

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `Gatsby blog`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-mdx", {
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-mdx", 'gatsby-plugin-postcss',{
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "pages",
